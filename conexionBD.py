6d0c850d80647fbcdd481f0442fa7c3e2f29d44c
Orlando <orladno142526@danielcastelao.org>
2021-04-13 10:43:49 +0200

    Modificacions de clase

--- conexionBD.py
+++ conexionBD.py
@@ -12,47 +12,8 @@ class ConexionBD:
         :param rutabd: Ruta onde se encontra o ficheiro da base de datos SQlite
         """
         self.rutaBd = rutabd
-        self.conexion = None
-        self.cursor = None
-
-        """
-        Codigo que crea a conexión da base de datos.
-        Para realizar a conexión precisa da ruta onde está a base de datos.
-        """
-
-        try:
-            if self.conexion is None:
-                if self.rutaBd is None:
-                    print("A ruta da base de datos é: None ")
-                else:
-                    self.conexion = dbapi.connect(self.rutaBd)
-            else:
-                print("Base de datos conectada: " + self.conexion)
-
-        except dbapi.StandardError as e:
-            print("Erro o facer a conexión a base de datos " + self.rutaBd + ": " + e)
-        else:
-            print("Conexión de base de datos realizada")
-
-        """
-        Codigo que crea o cursor da base de datos.
-        Para realizar o cursor precísase previamente da conexión da BD, que crea a conexión a base de
-        datos na ruta dada.
-        """
-
-        try:
-            if self.conexion is None:
-                print("Creando o cursor: É necesario realizar a conexión a base de datos previamente")
-            else:
-                if self.cursor is None:
-                    self.cursor = self.conexion.cursor()
-                else:
-                    print("O cursor xa esta inicializado: " + self.cursor)
-
-        except dbapi.Error as e:
-            print(e)
-        else:
-            print("Cursor preparado")
+        self.conexion = dbapi.connect(self.rutaBd)
+        self.cursor = self.conexion.cursor()
 
     def consultaSenParametros(self, consultaSQL):
         """
--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     # baseDatos.insertarRexistro("insert into CLIENTES values ('4', 'Xacobo', 'Piñeiro Cacableos', '986554775', 'calle',
     #                                                          'Vigo', 'Pontevedra', '36547', 'España', '1')")
     # print(baseDatos.consultaSenParametros("Select * from CLIENTES"))
-    baseDatos.modificarRexistros("update CLIENTES set axenteComercial = '2' where numeroCliente = '1'")
-    print(baseDatos.consultaSenParametros("Select * from CLIENTES"))
+    # baseDatos.modificarRexistros("update CLIENTES set axenteComercial = '2' where numeroCliente = '1'")
+    # print(baseDatos.consultaSenParametros("Select * from CLIENTES"))
